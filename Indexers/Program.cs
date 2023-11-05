307a00eb1174f140479451f62ba2e057cd5c3319
yrakhwas <yrakhwas@gmail.com>
2023-11-05 11:52:15 +0200

    Add project files.

---
+++ Indexers/Program.cs
@@ -0,0 +1,189 @@
+﻿using System.Diagnostics;
+
+Laptop l1 = new Laptop("Lenovo", 26000.00);
+Shop shop = new Shop(5);
+shop.SetLaptop(l1, 0);
+shop.SetLaptop(new Laptop("Asus", 600000.00), 1);
+//Console.WriteLine(shop.GetLaptop(0));
+
+shop[2] = new Laptop("Dell", 34000.00);
+shop[3] = new Laptop("HP", 40000.00);
+shop[4] = new Laptop("Apple", 43000.00);
+
+shop["HP"] = new Laptop("Xiaomi", 25000.00);
+
+shop[26000.00] = new Laptop("Dream Machine", 22000.00);
+for (int i = 0; i < shop.length; i++)
+{
+    Console.WriteLine(shop[i].ToString()) ;
+}
+
+
+MulArr mulArr = new MulArr(2, 2);
+mulArr[0, 0] = 58;
+for (int i = 0;i<mulArr.rows;i++)
+{
+    for (int j = 0;j < mulArr.cols;j++)
+    {
+        Console.Write(mulArr[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+class Laptop
+{
+    public string Brand { get; set; }
+    public double Price { get; set; }
+    public Laptop():base(){ }
+    public Laptop(string b, double p)
+    {
+        Brand = b;
+        Price = p;
+    }
+    public override string ToString()
+    {
+        return $"Brand : {Brand} Price : {Price}";
+    }
+}
+
+class Shop
+{
+    Laptop[] laptops;
+    public Shop(int size)
+    {
+        laptops = new Laptop[size];
+    }
+    public int length { get
+        {
+            return laptops.Length;
+        }
+    }
+    public void SetLaptop(Laptop laptop, int index)
+    {
+        laptops[index] = laptop;
+    }
+    public Laptop GetLaptop(int index)
+    {
+        if(index >=0 && index <=laptops.Length)
+            return laptops[index];
+        else
+            throw new IndexOutOfRangeException("Incorrect index");
+    }
+    public Laptop this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index <= laptops.Length)
+                return laptops[index];
+            else
+                throw new IndexOutOfRangeException("Incorrect index");
+        }
+        set
+        {
+            if (index >= 0 && index <= laptops.Length)
+                laptops[index] = value;
+            else
+                throw new IndexOutOfRangeException("Out of range");
+        }
+
+    }
+    public Laptop this[string brand]
+    {
+        get
+        {
+            foreach (var item in laptops)
+            {
+                if (brand == item.Brand)
+                {
+                    return item;
+                }
+                else
+                    return null;
+            }
+            return null;
+        }
+        set
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if (laptops[i].Brand == brand)
+                {
+                    laptops[i] = value;
+                    break;
+                }
+            }
+        }
+    }
+    public int FindByPrice(double Price)
+    {
+        for (int i = 0; i < laptops.Length; i++)
+        {
+            if (Price == laptops[i].Price)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public Laptop this[double price]
+    {
+        get
+        {
+            //foreach (var item in laptops)
+            //{
+            //    if (price == item.Price)
+            //    {
+            //        return item;
+            //    }
+            //    else
+            //        return null;
+            //}
+            //return null;
+            int index = FindByPrice(price);
+            if (index != -1)
+                return laptops[index];
+            else
+                throw new Exception("Incorrect price");
+        }
+        set
+        {
+            //for (int i = 0; i < laptops.Length; i++)
+            //{
+            //    if (laptops[i].Price == price)
+            //    {
+            //        laptops[i] = value;
+            //        break;
+            //    }
+            //}
+            int index = FindByPrice(price);
+            if(index != -1)
+            {
+                this[index] = value;
+            }
+        }
+    }
+}
+
+class MulArr
+{
+    private int[,] arr;
+    public int rows { get; set; }
+    public int cols { get; set; }
+    public MulArr(int r, int c)
+    {
+        rows = r; cols = c;
+        arr = new int[rows, cols];
+    }
+    public int this[int r, int c]
+    {
+        get
+        {
+            return arr[r, c];
+        }
+        set
+        {
+            arr[r, c] = value;  
+        }
+    }
+
+}
---
+++ OverloadingOperators.Indexers/Program.cs
@@ -0,0 +1,192 @@
+﻿using System.Security.Cryptography;
+
+Point p1 = new Point(2, 2);
+//Console.WriteLine(-p1);
+
+
+//Console.WriteLine(p1++);
+//Console.WriteLine(++p1);
+//Console.WriteLine(p1--);
+//Console.WriteLine(--p1);
+Point p2 = new Point(2,2);
+Console.WriteLine(p2!=p1);
+
+
+
+Point3D p3D = (Point3D)p2;
+Console.WriteLine(p3D);
+
+
+//if (p2)
+//{
+//    Console.WriteLine("This point is not null");
+//}
+
+class Point3D
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public int z { get; set; }
+    public Point3D(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public override string ToString()
+    {
+        return $"X: {x} Y : {y} Z : {z}";
+    }
+
+    //public override bool Equals(object? obj)
+    //{
+    //    Point p = obj as Point;
+    //    return this.x == p.x && this.y == p.y;
+
+
+    //    //var point = (Point)obj;
+    //    //return point!=null && this.x==point.x && this.y==point.y ;
+
+    //    //if(obj== null || GetType()!=obj.GetType())
+    //    //    return false;
+    //    //return true;
+    //}
+}
+    class Point
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public Point():base(){}
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    //public override string ToString()
+    //{
+    //    return $"X: {x} Y : {y}";
+    //}
+
+    public override bool Equals(object? obj)
+    {
+        Point p = obj as Point;
+        return this.x == p.x && this.y == p.y;
+
+
+        //var point = (Point)obj;
+        //return point!=null && this.x==point.x && this.y==point.y ;
+
+        //if(obj== null || GetType()!=obj.GetType())
+        //    return false;
+        //return true;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
+
+    #region Uno operators
+    public static Point operator-(Point p)
+    {
+        
+        p.x = -p.x;
+        p.y=-p.y;
+        return p;
+    }
+    public static Point operator ++(Point p)
+    {
+        ++p.x;
+        p.y++;
+        return p;
+    }
+    public static Point operator --(Point p)
+    {
+        return new Point(p.x--, p.y--);
+    }
+
+    #endregion
+
+    #region Binary operators
+
+    public static Point operator+(Point p1, Point p2)
+    {
+        Point p3 = new Point(p1.x + p2.x, p1.y + p2.y);
+        return p3;
+    }
+
+    public static Point operator -(Point p1, Point p2)
+    {
+        return new Point(p1.x - p2.x, p1.y - p2.y);
+    }
+    public static Point operator *(Point p1, Point p2)
+    {
+        Point p3 = new Point
+        {
+            x = p1.x * p2.x,
+            y = p1.y * p2.y
+        };
+        return p3;
+    }
+    public static Point operator /(Point p1, Point p2)
+    {
+        Point p3 = new Point
+        {
+            x = p1.x / p2.x,
+            y = p1.y / p2.y
+        };
+        return p3;
+    }
+
+    #endregion
+
+    #region
+    public static bool operator == (Point p1, Point p2)
+    {
+        return p1.Equals(p2);
+        //return p1.x == p1.y && p1.y == p2.y ? true : false;
+    }
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return p1.x != p1.y && p1.y != p2.y;
+    }
+
+    public static bool operator >(Point p1, Point p2)
+    {
+        return (p1.x + p1.y) > (p2.x + p2.y);
+    }
+    public static bool operator <(Point p1, Point p2)
+    {
+        return (p1.x + p1.y) < (p2.x + p2.y);
+    }
+    #endregion
+
+
+    public static bool operator true(Point p)
+    {
+        return p.x !=0 || p.y !=0;
+    }
+    public static bool operator false(Point p)
+    {
+        return p.x == 0 && p.y == 0;
+    }
+
+    //public static explicit operator int(Point p)
+    //{
+    //    return p.x + p.y;
+    //}
+    public static implicit operator int(Point p)
+    {
+        return p.x + p.y;
+    }
+
+    public static explicit operator string(Point p)
+    {
+        return p.ToString();
+    }
+    public static explicit operator Point3D(Point p)
+    {
+        return new Point3D(p.x, p.y, 0);
+    }
+
+}
